ddc0167e0920d90c468a5e16181dd03f79627cbc
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-04 22:48:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//store a reference of elements in a variable using query selector so that I can later use these elements in my code
 let productContainer = document.querySelector('section');
 let resultButton = document.querySelector('section + div');
 let image1 = document.querySelector('section img:first-child');
@@ -14,12 +15,13 @@ let uniqueImageCount = 6 // To make sure I display every possible combination of
 
 
 
-const state = { //
+const state = { // this object is used to store the state of the application as it runs
     allProductsArray: [],
     indexArray: [],
 };
 
 //functional logic
+//
 
 function duckProduct(name, path) {
     this.name = name;
@@ -28,16 +30,18 @@ function duckProduct(name, path) {
     this.votes = 0;
 }
 
-function getRandomNumber() {
+function getRandomNumber() { // returns a random number b/w 0 and the length of the allproducts aray
     return Math.floor(Math.random() * state.allProductsArray.length);
 }
 
 function renderProducts() {
-    //call getRandomNumber function
+    //selects 3 random images from the array of products and displays them on the page
 
     while (state.indexArray.length < uniqueImageCount) {
+   //loop runs until the state.index array's length equals the unique image count variable   
         let randomNumber = getRandomNumber();
-        if (!state.indexArray.includes(randomNumber)) { //if your boolean is true the ! flips it to false
+        if (!state.indexArray.includes(randomNumber)) {
+  // if checks if the state.index array  doesnt already include the random number, if true it adds the number to the array using .push method     
             state.indexArray.push(randomNumber);
         }
     }
@@ -46,30 +50,37 @@ function renderProducts() {
     let product1 = state.indexArray.shift(); //changes the length of the array
     let product2 = state.indexArray.shift();
     let product3 = state.indexArray.shift();
-
+//products 1-3 are assigned to the first 3 elements of the array using shift; 
 
    
-    image1.src = state.allProductsArray[product1].path;
+    image1.src = state.allProductsArray[product1].path; 
+    //src attribute sets the path property of the product objects 
     image2.src = state.allProductsArray[product2].path;
     image3.src = state.allProductsArray[product3].path;
 
     image1.alt = state.allProductsArray[product1].name;
+    //alt attribute sets the name property of the product objects
     image2.alt = state.allProductsArray[product2].name;
     image3.alt = state.allProductsArray[product3].name;
 
     state.allProductsArray[product1].timesImageViewed++;
+    //times image viewed incremented by 1 keeps track of how many times the image has been shown
     state.allProductsArray[product2].timesImageViewed++;
     state.allProductsArray[product3].timesImageViewed++;
     }
 
-
-function checkIfProductIsClicked(event) {
+//event handler function
+//counts the number of votes for each image and stores them in an array
+function checkIfProductIsClicked(event) { 
+  //if checks if the event is the product container,if som then the click was outside of the image and the user gets an alert
     if (event.target === productContainer) {
         alert('Not an image, click an image');
     }
     votes++;
 
     let clickProduct = event.target.alt;
+    //loop checks if the clicked products matches the name property of any object in the array
+    //if it does, the votes property incremented by 1 and breaks once theres a match
     for (let i = 0; i < state.allProductsArray.length; i++) {
         if (clickProduct === state.allProductsArray[i].name) {
             state.allProductsArray[i].votes++;
@@ -77,18 +88,20 @@ function checkIfProductIsClicked(event) {
         }
     }
     if (votes === maxVotesAllowed) {
+      //if user reachers 25 clicks, the event listeners listens for the removeevent listener
       productContainer.removeEventListener('click', checkIfProductIsClicked);
-      productContainer.className = 'no-voting';
-      printChart();
+      productContainer.className = 'no-voting'; //the no vote class disables more votes
+      printChart(); //results print once voting ends or it continues to render products
     } else {
       renderProducts();
     }
     
     localStorage.setItem('state.allProductsArray', JSON.stringify(state.allProductsArray));
-  
+  //local storage is used to save the updated array to the broswers local storage
 }
 
-function printChart(){
+
+function printChart(){ //prints the chart w/ the number of votes and times the images were shown
     let productNames = [];
     let productVotes = [];
     let productViews = [];
@@ -140,7 +153,7 @@ function printChart(){
 
 
 //executable code
-
+//creates new product objects
 let bag = new duckProduct('bag', 'img/bag.jpg');
 let banana = new duckProduct('banana', 'img/banana.jpg');
 let bathroom = new duckProduct('bathroom', 'img/bathroom.jpg')
@@ -161,12 +174,12 @@ let tauntaun = new duckProduct('tauntaun', 'img/tauntaun.jpg');
 if(localStorage['state.allProductsArray']){
   state.allProductsArray = JSON.parse(localStorage.getItem('state.allProductsArray'))
 } else{
-  state.allProductsArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun);
+  state.allProductsArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun); //
 }
 
 
 renderProducts();
-
+//event listener is added to the container elements that listens for the clicks on the images and triggers the checkifproduct is clicked fucntion
 productContainer.addEventListener('click', checkIfProductIsClicked);
 
 
